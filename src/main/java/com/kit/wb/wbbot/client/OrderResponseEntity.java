bcc7f1a88946e009c1f3c6845600238913af0cc1
nandreev <nandreev@avaya.com>
2022-05-23 19:01:48 +0300

    add json parser

--- src/main/java/com/kit/wb/wbbot/client/OrderResponseEntity.java
+++ src/main/java/com/kit/wb/wbbot/client/OrderResponseEntity.java
@@ -1,12 +1,75 @@
-/*
+
 package com.kit.wb.wbbot.client;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
-public class OrderResponseEntity {
+import java.io.Serializable;
+
+@Data
+public class OrderResponseEntity implements Serializable {
 
 	@JsonProperty("number")
 	private int number;
-	private String
+
+	@JsonProperty("date")
+	private String date;
+
+	@JsonProperty("lastChangeDate")
+	private String lastChangeDate;
+
+	@JsonProperty("supplierArticle")
+	private String supplierArticle;
+
+	@JsonProperty("techSize")
+	private String techSize;
+
+	@JsonProperty("barcode")
+	private String barcode;
+
+	@JsonProperty("quantity")
+	private long quantity;
+
+	@JsonProperty("totalPrice")
+	private long totalPrice;
+
+	@JsonProperty("discountPercent")
+	private long discountPercent;
+
+	@JsonProperty("warehouseName")
+	private String warehouseName;
+
+	@JsonProperty("oblast")
+	private String oblast;
+
+	@JsonProperty("incomeID")
+	private long incomeID;
+
+	@JsonProperty("odid")
+	private long odid;
+
+	@JsonProperty("nmId")
+	private long nmId;
+
+	@JsonProperty("subject")
+	private String subject;
+
+	@JsonProperty("category")
+	private String category;
+
+	@JsonProperty("brand")
+	private String brand;
+
+	@JsonProperty("isCancel")
+	private boolean isCancel;
+
+	@JsonProperty("cancel_dt")
+	private String cancel_dt;
+
+	@JsonProperty("gNumber")
+	private String gNumber;
+
+	@JsonProperty("sticker")
+	private String sticker;
+
 }
-*/
--- src/main/java/com/kit/wb/wbbot/handler/ClientHandler.java
+++ src/main/java/com/kit/wb/wbbot/handler/ClientHandler.java
@@ -5,15 +5,21 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.kit.wb.wbbot.client.ClientManager;
+import com.kit.wb.wbbot.client.OrderResponseEntity;
 import com.kit.wb.wbbot.client.RestClient;
 import com.kit.wb.wbbot.user.UserManager;
 import com.kit.wb.wbbot.user.UserState;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @AllArgsConstructor
+@Slf4j
 public class ClientHandler implements TelegramHandler {
 
 	RestClient wbClient;
@@ -29,12 +35,51 @@ public class ClientHandler implements TelegramHandler {
 				UserManager.updateState(message.getFrom(), UserState.NONE);
 				return "Этот раздел еще в разработке";
 			} else if(message.getText().equals("/orders")){
-				String res = wbClient.get("/api/v1/supplier/orders?dateFrom=2017-04-09T15:21:20&key="
-						+ ClientManager.getApi(message.getFrom()));
-				UserManager.updateState(message.getFrom(), UserState.NONE);
-				return res;
-				//JSONObject object = (JSONObject) new JSONParser().parse(res);
+				String res;
+				try {
+					res = wbClient.get("/api/v1/supplier/orders?dateFrom=2017-04-09T15:21:20&key="
+							+ ClientManager.getApi(message.getFrom()));
+				} catch (Exception e){
+					log.error("can not connect WB api", e);
+					return "нет соединения с WB: " + e.getMessage();
+				}
 
+				UserManager.updateState(message.getFrom(), UserState.NONE);
+
+				ObjectMapper mapper = new ObjectMapper();
+
+				List<OrderResponseEntity> list;
+				try {
+					list = mapper.readValue(res, mapper.getTypeFactory().
+							constructCollectionType(LinkedList.class, OrderResponseEntity.class));
+				} catch (JsonProcessingException e) {
+					log.warn("can not parse", e);
+					return "не удалось прочитать ответ";
+				}
+
+				StringBuilder answer = new StringBuilder("получено " + list.size() + " заказов:\n\n");
+
+				for (int i = 0; i < list.size(); i++) {
+					answer.append("заказ ");
+					answer.append(i + 1);
+					answer.append("\nдата:    ");
+					answer.append(list.get(i).getDate());
+					answer.append("\nартикул: ");
+					answer.append(list.get(i).getSupplierArticle());
+					answer.append("\nразмер:  ");
+					answer.append(list.get(i).getTechSize());
+					answer.append("\nbarcode: ");
+					answer.append(list.get(i).getBarcode());
+					answer.append("\nцена:    ");
+					long endPrice = (100 - list.get(i).getDiscountPercent()) * list.get(i).getTotalPrice();
+					answer.append(endPrice);
+					answer.append("\nобласть: ");
+					answer.append(list.get(i).getOblast());
+					answer.append("\n-------");
+				}
+
+				log.info(answer.toString());
+				return answer.toString();
 			}
 
 		}
